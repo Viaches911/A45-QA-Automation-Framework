e2830e48d0a30f75e50628a3a1cca88a6be4719d
Viaches911 <131311110+Viaches911@users.noreply.github.com>
2023-07-03 14:06:23 -0400

    Summary

--- src/test/java/HomeWork16.java
+++ src/test/java/HomeWork16.java
@@ -1,8 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,16 +8,6 @@ import java.time.Duration;
 public class HomeWork16 extends BaseTest {
     @Test
     public void registrationNavigation()  {
-        //Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
 
         WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
         registrationLink.click();
@@ -28,6 +15,5 @@ public class HomeWork16 extends BaseTest {
         String registrationUrl = "https://qa.koel.app/registration.php";
         Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
 
-        driver.quit();
     }
 }
