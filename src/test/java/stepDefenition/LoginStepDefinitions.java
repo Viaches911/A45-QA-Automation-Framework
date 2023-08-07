ba395b9b3a5576a59bb7e64cbdfac64325d6bb30
Viaches911 <131311110+Viaches911@users.noreply.github.com>
2023-08-07 13:51:47 -0400

    Summary

--- src/test/java/stepDefenition/LoginStepDefinitions.java
+++ src/test/java/stepDefenition/LoginStepDefinitions.java
@@ -21,7 +21,7 @@ public class LoginStepDefinitions {
     WebDriver driver;
     WebDriverWait wait;
 
-    @Before("I open browser")
+    @Before
     public void openBrowser() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -31,31 +31,35 @@ public class LoginStepDefinitions {
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+
     @Given("I open Login Page")
     public void openLoginPage() {
         driver.get("https://qa.koel.app");
     }
+
     @When("I enter email {string}")
-    public void i_enter_email(String email){
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys((email));
+    public void i_enter_email(String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys((email));
     }
+
     @And("I enter password {string}")
-    public void i_enter_password(String password){
+    public void i_enter_password(String password) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']"))).sendKeys(password);
     }
+
     @And("I submit")
-    public void clickSubmit(){
+    public void clickSubmit() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']"))).click();
     }
 
     @Then("I am logged in")
-    public void userIsLoggedIn(){
+    public void userIsLoggedIn() {
         Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
     }
-
-    @After
-    public void closeBrowser(){
-        driver.quit();
-    }
-
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,10 +1,8 @@
 Feature: Login feature
 
   Scenario: Login Success
-   #Before
     Given I open Login Page
     When I enter email "demo@class.com"
     And I enter password "te$t$tudent"
     And I submit
     Then I am logged in
-    #After
